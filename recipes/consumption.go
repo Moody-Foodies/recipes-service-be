45a544c56749ba381b303a95c079faff7689cd61
Jack Sweeney <jackcsweeney@gmail.com>
2024-05-13 15:18:48 -0700

    Change log.Fatal to print line to test if error was breaking code

--- recipes/consumption.go
+++ recipes/consumption.go
@@ -14,7 +14,7 @@ import (
 func MakeRequest(c *gin.Context, nutrient string, cookTime string) {
 	err := godotenv.Load()
 	if err != nil{
-		log.Fatal("Error loading.env file")
+		fmt.Println("Error loading.env file")
 	}
 	apiKey := os.Getenv("API_KEY")
 
